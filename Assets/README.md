5c4801eeed70366497fb41277217c0a7e6edac0a
Danny Collan <collan97@gmail.com>
2023-10-08 13:11:46 -0700

    Added questions

---
+++ Assets/README.md
@@ -0,0 +1,36 @@
+
+    # Project Title
+    Some new project
+    [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)] // Include the license badge here
+
+    # Description
+    dscvd
+
+    # Table of Contents 
+    * [Installation](#-Installation)
+    * [Usage](#-Usage)
+    * [License](#-License)
+    * [Contributing](#-Contributing)
+    * [Tests](#-Tests)
+    * [Questions](#-Contact-Information)
+
+    # Installation
+    vdfv
+
+    # Usage
+    vgfg
+
+    
+    # License
+    This project is licensed under the [Apache 2.0](https://opensource.org/licenses/Apache-2.0) license.
+    
+
+    # Contributing 
+    vfvf
+
+    # Tests
+    vdfvd
+
+    # Contact Information 
+    * GitHub Username: fvd
+    * Contact Email: vfdv
--- index.js
+++ index.js
@@ -1,13 +1,95 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// need inquirer variable here
+const inquirer = require("inquirer");
+
+// need a markdown js file here
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+const questions = [
+    
+    // Project title
+    {
+        type: "input",
+        name: "title",
+        message: "Project title: ",
+    },
+
+    // Project description
+    {
+        type: "input",
+        name: "description",
+        message: "Project description: ",
+    },
+
+    // Installation
+    {
+        type: "input",
+        name: "installation",
+        message: "How to install: ",
+    },
+
+    // How to use
+    {
+        type: "input",
+        name: "usage",
+        message: "How to use application: ",
+    },
+
+    // Select license
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for the project: ",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Apache 2.0",
+            "Boost Software 1.0",
+            "MIT",
+            "Mozilla",
+        ],
+    },
+
+    // Contribution
+    {
+        type: "input",
+        name: "contributing",
+        message: "How to contribute to project: ",
+    },
+
+    // How to test
+    {
+        type: "input",
+        name: "tests",
+        message: "How to test the project: ",
+    },
+
+    // Github Username
+    {
+        type: "input",
+        name: "userName",
+        message: "Github Username: ",
+    },
+
+    // Email
+    {
+        type: "input",
+        name: "userEmail",
+        message: "Email: ",
+    },
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
+
 // TODO: Create a function to initialize app
 function init() {}
+
 
 // Function call to initialize app
 init();
