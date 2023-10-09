54a3a552bfd805534546aee56ea9386942ed742c
Danny Collan <collan97@gmail.com>
2023-10-08 18:02:18 -0700

    Completed renderLicenseSection() to return license information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -68,14 +68,24 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license) {
+    // Receive license name and license link by calling renderLicenseLink() function
+    return `
+    # License
+    You are currently using the [${license}](${renderLicenseLink(license)}) license.
+    `;
+    } else {
+        return "";
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
-
+    return `# ${data.title}
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
